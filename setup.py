67ef3684de02c963afa904046f566658060636de
abelsonlive <brianabelson@gmail.com>
2014-01-21 23:36:53 -0500

    whups

--- setup.py
+++ setup.py
@@ -23,9 +23,17 @@ setup(
     zip_safe = False,
     install_requires = [
         "Cython==0.19.2",
-         "python-rtmidi==0.4.3b1",
-         "tweepy==2.1",
-         "wsgiref==0.1.2"
-    ],
+        "Pillow==2.3.0",
+        "PyYAML==3.10",
+        "nltk==2.0.4",
+        "python-rtmidi==0.4.3b1",
+        "requests==2.2.0",
+        "six==1.5.2",
+        "socketIO-client==0.5.3",
+        "tweepy==2.1",
+        "tweet-music==0.0.2",
+        "websocket-client==0.12.0",
+        "wsgiref==0.1.2"
+        ],
     tests_require = []
 )
